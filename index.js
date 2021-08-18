78f60599f04c3f4ebe18d16274bfccb364c415de
pkIRIS <61252062+pkIRIS@users.noreply.github.com>
2021-08-18 13:47:50 +0200

    1st

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
